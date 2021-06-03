e5d3b35cffca24a1e2a909265f8e0b57386461f7
pooja v <pooja.v@mobiotics.com>
2021-06-03 15:56:35 +0530

    Code files related to the API to move the selected file to S3

---
+++ submit.php
@@ -0,0 +1,57 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+require __DIR__ . '/google-drive.php';
+
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+$service = new Google_Service_Drive($GLOBALS['client']);
+
+$folderName = $_POST['folder'];
+$parameters['q'] = "name = '" . $folderName . "' and  trashed=false";
+$files = $service->files->listFiles($parameters);
+
+
+$url = getcwd() . '/images/' . $_POST['filename'];
+
+try {
+
+    $bucketName = $_POST['s3bucket'];
+    $IAM_KEY = '';
+    $IAM_SECRET = '';
+
+
+    $s3 = S3Client::factory(
+        array(
+            'credentials' => array(
+                'key' => $IAM_KEY,
+                'secret' => $IAM_SECRET
+            ),
+            'version' => 'latest',
+            'region'  => 'ap-south-1'
+        )
+    );
+} catch (Exception $e) {
+
+    die("Error: " . $e->getMessage());
+}
+
+$keyName = 'test_example/' . $_POST['filename'];
+$pathInS3 = 'https://s3.ap-south-1.amazonaws.com/' . $bucketName . '/' . $keyName;
+$result = $s3->putObject(
+    array(
+        'Bucket' => $bucketName,
+        'Key' =>  $keyName,
+        'SourceFile' => $url,
+        'ACL'        => 'public-read', //for making the public url
+        'StorageClass' => 'REDUCED_REDUNDANCY'
+    )
+);
+
+$url = $result->get('ObjectURL');
+echo "<br>Image uploaded successfully. Image path is: " . $result->get('ObjectURL');
+$response = ['success'=>$_POST['filename']];
+echo json_encode($response);
+
